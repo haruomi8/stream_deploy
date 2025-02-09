83de5d69e3800b9844c65efb904fc40f346bfb70
haru <jesuisharu03@gmail.com>
2025-02-09 11:52:32 +0900

    text more joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-if st.button("hello"):
-    st.text("hello i going to your house good bye")
+name = st.text_input("wat's your name")
+if st.button("input"):
+    st.text(f"hello {name:.2f} i going to your house good bye(don't worry this is fake)")
     if st.button("delete"):
         st.text(" ")
