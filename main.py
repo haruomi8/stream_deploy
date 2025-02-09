fb2469b550b74c85175f3574c2d2e39dab2fa0ff
haru <jesuisharu03@gmail.com>
2025-02-09 12:21:24 +0900

    text more safe 2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 name = st.text_input("wat's your name")
 if st.button("input"):
-    st.text(f"hello {name} nice to meet you  good bye   (don't worry this is fake)")
+    st.text(f"hello {name} nice to meet you  good bye")
     if st.button("delete"):
         st.text(" ")
